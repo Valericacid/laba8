677afd2dd1cf05c8d63d6fe29390ca6b11b9a109
Valericacid <92182164+Valericacid@users.noreply.github.com>
2021-11-20 19:59:04 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+
+def kbig(nums, k):
+
+    count = []
+    for e in nums:
+        if e + 1 > len(count):
+            for i in range(e - len(count) +1 ):
+                count.append(0)
+        count[e] += 1
+
+    d = 1
+    l = len(count)
+    res = l - 1
+
+    for i in range(k):
+        if count[l-d]==0:
+            res -= 1
+            d += 1
+        count[l - d] -= 1
+
+    return res
+
+if __name__ == '__main__':
+
+    a = [3,2,3,1,2,4,5,5,6]
+    print(kbig(a, 9))
+
+
+
